2b7934975079ec1d0e1a25a4fa1fe08d9b47dcca
Wang Haoyu <WangHaoyu-geren@outlook.com>
2026-02-21 01:11:11 +0800

    1.3.0.0

--- Plugin.cs
+++ Plugin.cs
@@ -1,12 +1,13 @@
-﻿using AvaloniaEdit.Utils;
-using ClassIsland.Core.Abstractions;
+﻿using ClassIsland.Core.Abstractions;
 using ClassIsland.Core.Abstractions.Services;
 using ClassIsland.Core.Extensions.Registry;
 using ClassIsland.Core.Models.Automation;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using VoiceAction.Actions;
-using VoiceAction.Controls;
+using VoiceAction.UN.Actions;
+using VoiceAction.SET.Actions;
+using VoiceAction.M.Actions;
+using AvaloniaEdit.Utils;
 
 namespace VoiceAction;
 
@@ -14,18 +15,24 @@ public class Plugin : PluginBase
 {
     public override void Initialize(HostBuilderContext context, IServiceCollection services)
     {
+        // ========== 构建行动树 ==========
+        BuildActionTree();
 
-        //行动树
-        IActionService.ActionMenuTree.Add(new ActionMenuTreeGroup("设置音量", "\uF019"));
-        IActionService.ActionMenuTree["设置音量"].AddRange([
-            new ActionMenuTreeItem("VoiceAction.AdjustVolume", "调整音量", "\uF013"),
-            new ActionMenuTreeItem("VoiceAction.Mute", "静音", "\uF015"),
-            new ActionMenuTreeItem("VoiceAction.Unmute", "取消静音", "\uF00D")
+        // ========== 注册行动 ==========
+        services.AddAction<SetVolumeAction, SetVolumeSettingsControl>();
+        services.AddAction<MuteVolumeAction>();
+        services.AddAction<UnmuteVolumeAction>();
+    }
+
+    private void BuildActionTree()
+    {
+        IActionService.ActionMenuTree.Add(
+            new ActionMenuTreeGroup("系统音量控制…", "\uF013"));
+
+        IActionService.ActionMenuTree["系统音量控制…"].AddRange([
+            new ActionMenuTreeItem("VoiceAction.AdjustVolume", "设置系统音量", "\uF013"),
+            new ActionMenuTreeItem("VoiceAction.Mute", "系统静音", "\uF015"),
+            new ActionMenuTreeItem("VoiceAction.Unmute", "取消系统静音", "\uF00D")
         ]);
-
-
-        services.AddAction<AdjustVolumeAction, AdjustVolumeSettingsControl>();
-        services.AddAction<MuteAction>();
-        services.AddAction<UnmuteAction>();
     }
 }
